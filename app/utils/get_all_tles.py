887b7bc1418a778f6bba774c1e01c397aa417a96
noahhh09 <241513985+noahhh09@users.noreply.github.com>
2026-01-23 16:54:31 +0000

    Add database support

--- app/utils/get_all_tles.py
+++ app/utils/get_all_tles.py
@@ -1,39 +1,70 @@
 from flask import Flask
 import requests
+import pandas
+import numpy as np
 from datetime import datetime, timedelta
 from sqlalchemy import create_engine
 
-ALL_TLES_ENDPOINT = "https://celestrak.org/NORAD/elements/gp.php?GROUP=ACTIVE&FORMAT=tle"
-ALL_TLES_UPDATE_RATE = timedelta(hours=2)
+# All related to 25544: https://celestrak.org/NORAD/elements/gp.php?INTDES=1998-067&FORMAT=json
+# 25544 only: https://celestrak.org/NORAD/elements/gp.php?CATNR=25544&FORMAT=tle
 
-# TODO: Use a database instead
-all_tle_cache = {
-    "lastUpdated": None,
-    "data": {}
-}
+ALL_TLES_ENDPOINT = f"https://celestrak.org/NORAD/elements/gp.php?GROUP=ACTIVE&FORMAT=tle"
+ALL_TLES_UPDATE_RATE = timedelta(hours=2)
 
 app = Flask(__name__)
 db = create_engine("sqlite:///tles.db")
+DB_COLUMNS = ["header", "line1", "line2"]
 
-# TODO: Make the database here
-# ? Consider wether to use pandas or class based columns, or SQL queries
-# ? Also consider TLEs vs OMM for future proofing
-
+# TODO: Split into functions
 def get_all_tles():
-    if all_tle_cache["lastUpdated"] is not None and datetime.now() - all_tle_cache["lastUpdated"] <= ALL_TLES_UPDATE_RATE:
+    last_updated = pandas.read_sql(
+        "SELECT value FROM metadata WHERE key='all_tles_last_updated'", 
+        db
+    )["value"].iloc[0]
+
+    if datetime.now() - datetime.fromisoformat(last_updated) <= ALL_TLES_UPDATE_RATE:
         print("Returning cached data")
-        return all_tle_cache["data"]
+
+        tle_df = pandas.read_sql("SELECT * from tles", db)
+        return tle_df[DB_COLUMNS].to_numpy().tolist()
     
     print("Requesting fresh data from", ALL_TLES_ENDPOINT)
     response = requests.get(ALL_TLES_ENDPOINT)
     if response.status_code == 200:
-        data = response.text
+        data = response.text.splitlines()
 
-        all_tle_cache["data"] = data
-        all_tle_cache["lastUpdated"] = datetime.now()
-        print("Returning data")
-        print(data)
-        return data
+        data = np.array(data)
+        chunked = data.reshape(-1, 3).tolist()
+
+        rows = []
+        for header, line1, line2 in chunked:
+            rows.append({
+                "header": header,
+                "line1": line1,
+                "line2": line2
+            })
+
+        df = pandas.DataFrame(rows, columns=DB_COLUMNS)
+        df.to_sql(
+            "tles",
+            con=db,
+            if_exists="replace",
+            index=False
+        )
+
+        metadata = pandas.DataFrame([{
+            "key": "all_tles_last_updated",
+            "value": datetime.now().isoformat()
+        }])
+        metadata.to_sql(
+            "metadata",
+            con=db,
+            if_exists="replace",
+            index=False
+        )
+
+        return chunked
+
     else:
         return {"error": "Failed to retrieve TLE data"}
     
