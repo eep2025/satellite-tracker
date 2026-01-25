913b0e2ab1828af0281cb25a00ef5bebd66b55eb
noahhh09 <241513985+noahhh09@users.noreply.github.com>
2026-01-25 20:01:49 +0000

    Change endpoint from /api/all_tles to /all_tles

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from utils.get_all_tles import get_all_tles
 
 app = Flask(__name__)
 
-@app.route("/api/all_tles")
+@app.route("/all_tles")
 def index():
     data, status_code = get_all_tles()
     return jsonify(data), status_code
